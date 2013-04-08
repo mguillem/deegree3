60fe0d42230f9c96afa958173f3536b065917069
aerben <erben@lat-lon.de>
2013-04-08 17:31:43 +0200

    Added support for securing the GET Operation on GetRecords and GetRecordById

---
+++ deegree-client/deegree-jsf-console-security/src/main/java/org/deegree/console/security/HttpGetSimpleFilter.java
@@ -0,0 +1,61 @@
+package org.deegree.console.security;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ * Generic HTTP GET Filter for Spring Security
+ * 
+ * @author <a href="mailto:erben@lat-lon.de">Alexander Erben</a>
+ * @author <a href="mailto:stenger@lat-lon.de">Dirk Stenger</a>
+ * 
+ */
+public class HttpGetSimpleFilter implements HttpGetFilter {
+
+    private final String allowedRole;
+    
+    private final String matchMethod;
+
+    public HttpGetSimpleFilter( String matchMethod, String allowedRole ) {
+        this.allowedRole = allowedRole;
+        this.matchMethod = matchMethod;
+    }
+
+    @Override
+    public boolean canHandle( String requestUrl, Map<String, String[]> paramMap ) {
+        String operation = retrieveFirstValueOfKey( paramMap, "request" );
+        return matchMethod.equalsIgnoreCase( operation );
+    }
+
+    @Override
+    public boolean isPermitted( String requestUrl, Map<String, String[]> paramMap, Authentication authentication ) {
+        if ( !canHandle( requestUrl, paramMap ) )
+            return false;
+        for ( GrantedAuthority auth : authentication.getAuthorities() ) {
+            System.out.println( auth.getAuthority() );
+            if ( allowedRole.equals( auth.getAuthority() ) )
+                return true;
+        }
+        return false;
+    }
+
+    private String retrieveFirstValueOfKey( Map<String, String[]> paramMap, String checkString ) {
+        Iterator<String> i = paramMap.keySet().iterator();
+        while ( i.hasNext() ) {
+            String key = (String) i.next();
+            String value = ( (String[]) paramMap.get( key ) )[0];
+            if ( checkString.equals( key.toLowerCase() ) ) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    public String getAllowedRole() {
+        return allowedRole;
+    }
+
+}
---
+++ deegree-client/deegree-jsf-console-security/src/test/java/org/deegree/console/security/HttpGetFilterTest.java
@@ -0,0 +1,44 @@
+package org.deegree.console.security;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class HttpGetFilterTest {
+
+    private static final String GETRECORDS_OPERATION = "getrecords";
+    
+    private static final String RECORDS_ROLE = "records";
+    
+    private final HttpGetFilter recordsFilter = new HttpGetSimpleFilter(GETRECORDS_OPERATION,RECORDS_ROLE);
+
+    @Test
+    public void testCanHandle() {
+        String requestUrl = "http://foo.bar/services/csw";
+        Map<String, String[]> paramMap = new HashMap<String, String[]>();
+        String[] params = { "GetRecords" };
+        paramMap.put( "request", params );
+        assertTrue( recordsFilter.canHandle( requestUrl, paramMap ) );
+    }
+
+    @Test
+    public void testCanHandleFalseOperation() {
+        String requestUrl = "http://foo.bar/services/csw";
+        Map<String, String[]> paramMap = new HashMap<String, String[]>();
+        String[] params = { "GetCapabilities" };
+        paramMap.put( "request", params );
+        assertFalse( recordsFilter.canHandle( requestUrl, paramMap ) );
+    }
+
+    @Test
+    public void testCanHandleDoubleKVP() {
+        String requestUrl = "http://foo.bar/services/csw";
+        Map<String, String[]> paramMap = new HashMap<String, String[]>();
+        String[] params = { "GetRecords", "GetRecordById" };
+        paramMap.put( "request", params );
+        assertTrue( recordsFilter.canHandle( requestUrl, paramMap ) );
+    }
+}
