852e40412aab390011b964186766d2c5f255287e
Lyn Goltz <goltz@lat-lon.de>
2014-09-08 17:35:43 +0200

    fixed transparence of gif images

--- deegree-core/deegree-core-rendering-2d/src/main/java/org/deegree/rendering/r2d/context/DefaultRenderContext.java
+++ deegree-core/deegree-core-rendering-2d/src/main/java/org/deegree/rendering/r2d/context/DefaultRenderContext.java
@@ -52,8 +52,13 @@ import static java.awt.RenderingHints.VALUE_TEXT_ANTIALIAS_ON;
 import static javax.imageio.ImageIO.write;
 import static org.apache.commons.io.IOUtils.closeQuietly;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.IndexColorModel;
+import java.awt.image.WritableRaster;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -132,6 +137,9 @@ public class DefaultRenderContext implements RenderContext {
             graphics.dispose();
             if ( out != null ) {
                 String format = this.format.substring( this.format.indexOf( "/" ) + 1 );
+                if ( format.equals( "gif" ) ) {
+                    return write( convertRGBAToIndexed( image ), format, out );
+                }
                 if ( format.equals( "x-ms-bmp" ) ) {
                     format = "bmp";
                 }
@@ -206,6 +214,34 @@ public class DefaultRenderContext implements RenderContext {
             graphics.setRenderingHint( KEY_RENDERING, VALUE_RENDER_DEFAULT );
             break;
         }
+    }
+
+    public static BufferedImage convertRGBAToIndexed( BufferedImage src ) {
+        BufferedImage dest = new BufferedImage( src.getWidth(), src.getHeight(), BufferedImage.TYPE_BYTE_INDEXED );
+        Graphics g = dest.getGraphics();
+        g.setColor( new Color( 231, 20, 189 ) );
+        g.fillRect( 0, 0, dest.getWidth(), dest.getHeight() ); // fill with a hideous color and make it transparent
+        dest = makeTransparent( dest, 0, 0 );
+        dest.createGraphics().drawImage( src, 0, 0, null );
+        return dest;
+    }
+
+    public static BufferedImage makeTransparent( BufferedImage image, int x, int y ) {
+        ColorModel cm = image.getColorModel();
+        if ( !( cm instanceof IndexColorModel ) )
+            return image; // sorry...
+        IndexColorModel icm = (IndexColorModel) cm;
+        WritableRaster raster = image.getRaster();
+        int pixel = raster.getSample( x, y, 0 ); // pixel is offset in ICM's palette
+        int size = icm.getMapSize();
+        byte[] reds = new byte[size];
+        byte[] greens = new byte[size];
+        byte[] blues = new byte[size];
+        icm.getReds( reds );
+        icm.getGreens( greens );
+        icm.getBlues( blues );
+        IndexColorModel icm2 = new IndexColorModel( 8, size, reds, greens, blues, pixel );
+        return new BufferedImage( icm2, raster, image.isAlphaPremultiplied(), null );
     }
 
 }
