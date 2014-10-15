9ba609d9b9592d34f51820af490d94d0ffb0d901
Lyn Goltz <goltz@lat-lon.de>
2014-10-15 10:36:48 +0200

    first transformaion approach

--- deegree-core/deegree-core-rendering-2d/src/main/java/org/deegree/rendering/r2d/Java2DTileRenderer.java
+++ deegree-core/deegree-core-rendering-2d/src/main/java/org/deegree/rendering/r2d/Java2DTileRenderer.java
@@ -41,14 +41,27 @@
 package org.deegree.rendering.r2d;
 
 import static java.awt.Color.RED;
+import static org.deegree.coverage.raster.geom.RasterGeoReference.OriginLocation.OUTER;
+import static org.deegree.coverage.raster.interpolation.InterpolationType.BILINEAR;
+import static org.deegree.coverage.raster.utils.RasterFactory.rasterDataFromImage;
+import static org.deegree.coverage.raster.utils.RasterFactory.rasterDataToImage;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
 
 import org.deegree.commons.utils.math.MathUtils;
+import org.deegree.coverage.raster.RasterTransformer;
+import org.deegree.coverage.raster.SimpleRaster;
+import org.deegree.coverage.raster.data.RasterData;
+import org.deegree.coverage.raster.geom.RasterGeoReference;
+import org.deegree.cs.coordinatesystems.ICRS;
+import org.deegree.cs.exceptions.TransformationException;
+import org.deegree.cs.exceptions.UnknownCRSException;
 import org.deegree.geometry.Envelope;
+import org.deegree.geometry.GeometryTransformer;
 import org.deegree.tile.Tile;
 import org.deegree.tile.TileIOException;
 import org.slf4j.Logger;
@@ -70,6 +83,12 @@ public class Java2DTileRenderer implements TileRenderer {
 
     private AffineTransform worldToScreen = new AffineTransform();
 
+    private Envelope envelope;
+
+    private int width;
+
+    private int height;
+
     /**
      * @param graphics
      * @param width
@@ -78,6 +97,24 @@ public class Java2DTileRenderer implements TileRenderer {
      */
     public Java2DTileRenderer( Graphics2D graphics, int width, int height, Envelope envelope ) {
         this.graphics = graphics;
+        this.width = width;
+        this.height = height;
+        this.envelope = envelope;
+        //
+        // try {
+        // ICRS targetCRS = CRSManager.lookup( "epsg:25833" );
+        // envelope = new GeometryTransformer( targetCRS ).transform( envelope );
+        // } catch ( IllegalArgumentException e ) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // } catch ( TransformationException e ) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // } catch ( UnknownCRSException e ) {
+        // // TODO Auto-generated catch block
+        // e.printStackTrace();
+        // }
+        //
         RenderHelper.getWorldToScreenTransform( worldToScreen, envelope, width, height );
     }
 
@@ -89,6 +126,35 @@ public class Java2DTileRenderer implements TileRenderer {
         }
         int minx, miny, maxx, maxy;
         Envelope env = tile.getEnvelope();
+
+        BufferedImage imageToDraw = tile.getAsImage();
+        ICRS requestedCrs = envelope.getCoordinateSystem();
+        if ( !requestedCrs.equals( env.getCoordinateSystem() ) ) {
+            try {
+                int tileWidth = imageToDraw.getWidth();
+                int tileHeight = imageToDraw.getHeight();
+
+                RasterGeoReference rasterGeoReference = RasterGeoReference.create( OUTER, env, tileWidth, tileHeight );
+                RasterData data = rasterDataFromImage( imageToDraw );
+                SimpleRaster raster = new SimpleRaster( data, env, rasterGeoReference, null );
+
+                RasterTransformer rtrans = new RasterTransformer( requestedCrs );
+                SimpleRaster transformed = rtrans.transform( raster, envelope, tileWidth, tileHeight, BILINEAR ).getAsSimpleRaster();
+                imageToDraw = rasterDataToImage( transformed.getRasterData() );
+
+                env = new GeometryTransformer( requestedCrs ).transform( env );
+            } catch ( TransformationException e ) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch ( IllegalArgumentException e ) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch ( UnknownCRSException e ) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
         Point2D.Double p = (Point2D.Double) worldToScreen.transform( new Point2D.Double( env.getMin().get0(),
                                                                                          env.getMin().get1() ), null );
         minx = MathUtils.round( p.x );
@@ -97,8 +163,13 @@ public class Java2DTileRenderer implements TileRenderer {
                                                       null );
         maxx = MathUtils.round( p.x );
         maxy = MathUtils.round( p.y );
+
+        int minxa = Math.min( minx, maxx );
+        int minya = Math.min( miny, maxy );
+        int maxxa = Math.max( minx, maxx );
+        int maxya = Math.max( miny, maxy );
         try {
-            graphics.drawImage( tile.getAsImage(), minx, miny, maxx - minx, maxy - miny, null );
+            graphics.drawImage( tile.getAsImage(), minxa, minya, maxxa - minxa, maxya - minya, null );
         } catch ( TileIOException e ) {
             LOG.debug( "Error retrieving tile image: " + e.getMessage() );
             graphics.setColor( RED );
